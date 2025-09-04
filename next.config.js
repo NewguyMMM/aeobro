b9a3de36dc9f999cf784bafcb0035b83c88140e2
NewguyMMM <erodriguez2004@hotmail.com>
2025-09-04 07:02:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports={};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/sign-in", destination: "/login", permanent: false },
+      { source: "/sign-up", destination: "/login", permanent: false },
+      { source: "/signin", destination: "/login", permanent: false },
+      { source: "/signup", destination: "/login", permanent: false },
+      { source: "/auth/sign-in", destination: "/login", permanent: false },
+      { source: "/auth/sign-up", destination: "/login", permanent: false },
+    ];
+  },
+};
+
+module.exports = nextConfig;
