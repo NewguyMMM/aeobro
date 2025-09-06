8bd35419237ea0892aa49d1ecfa352780d8f6290
NewguyMMM <erodriguez2004@hotmail.com>
2025-09-06 16:50:28 -0400

    Update auth-server.ts

--- lib/auth-server.ts
+++ lib/auth-server.ts
@@ -1,5 +1,6 @@
+// lib/auth-server.ts
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function auth() {
   return getServerSession(authOptions);
